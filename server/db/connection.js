f82ffd93630235a7aa54aacab07c09fef8771db6
FredderDev <freddermendez44@gmail.com>
2026-08-21 23:44:47 -0600

    Respaldo agregado

--- server/db/connection.js
+++ server/db/connection.js
@@ -1,6 +1,6 @@
 // parametros de conexion inicial
 
-host=db.apexlbtxgewjesqewdtw.supabase.co
+host=db.apexlbtxgewjesqewdtw.supabase.co // host de conexion respaldo
 port=5432
 database=postgres
 user=postgres
